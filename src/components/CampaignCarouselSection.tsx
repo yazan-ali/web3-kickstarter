3e906b8a57a9d2986db38ef8f7510b2e5705bb67
yazan ali <yazanabuali2000@gmail.com>
2025-02-16 17:02:35 +0300

    Fix server side rendering issue

--- src/components/CampaignCarouselSection.tsx
+++ src/components/CampaignCarouselSection.tsx
@@ -1,9 +1,11 @@
 import factory from "@ethereum/factory";
 import CampaignCarousel from "./CampaignCarousel";
+import { unstable_noStore as noStore } from "next/cache";
 
 export const dynamic = "force-dynamic";
 
 async function CampaignCarouselSection() {
+  noStore();
   const campaigns = await factory.methods.getDeployedCampaigns().call();
   return <CampaignCarousel campaigns={campaigns.slice(0, 5)} />;
 }
