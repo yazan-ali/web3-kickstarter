bd51061c8262d761bcdf2d32b8c626a247aee64a
yazan ali <yazanabuali2000@gmail.com>
2025-02-16 17:20:30 +0300

    Fix an issue with the approve button

--- src/components/RequestApproveBtn.tsx
+++ src/components/RequestApproveBtn.tsx
@@ -1,46 +1,52 @@
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
-import web3 from '@ethereum/web3';
-import { getCampaign } from '@ethereum/campaign';
+import web3 from "@ethereum/web3";
+import { getCampaign } from "@ethereum/campaign";
 
 interface RequestApproveBtnProps {
-    handleRequestApprove: () => void;
-    address: any;
-    isCompleted: boolean;
-    isLoading: boolean;
+  handleRequestApprove: () => void;
+  address: any;
+  isCompleted: boolean;
+  isLoading: boolean;
 }
 
-function RequestApproveBtn({ handleRequestApprove, address, isCompleted, isLoading }: RequestApproveBtnProps) {
+function RequestApproveBtn({
+  handleRequestApprove,
+  address,
+  isCompleted,
+  isLoading,
+}: RequestApproveBtnProps) {
+  const [didApprove, setDidApprove] = useState(false);
 
-    const [canApprove, setCanApprove] = useState(false);
+  useEffect(() => {
+    const isApprover = async () => {
+      let campaign = getCampaign(address);
+      let accounts = await web3.eth.getAccounts();
+      let didApprove = await campaign.methods.approvers(accounts[0]).call();
+      setDidApprove(didApprove);
+    };
+    isApprover();
+  }, []);
 
-    useEffect(() => {
-        const isApprover = async () => {
-            let campaign = getCampaign(address);
-            let accounts = await web3.eth.getAccounts();
-            let canApprove = await campaign.methods.approvers(accounts[0]).call();
-            setCanApprove(canApprove);
-        }
-        isApprover();
-
-    }, []);
-
-    return (
-        <>
-            {
-                isLoading ? (
-                    <Button disabled className="my-4">
-                        Loading <Loader2 className="animate-spin" />
-                    </Button>
-                ) : (
-                    <Button onClick={handleRequestApprove} type="button" className="bg-green-700" disabled={!canApprove || isCompleted}>
-                        Approve
-                    </Button>
-                )
-            }
-        </>
-    )
+  return (
+    <>
+      {isLoading ? (
+        <Button disabled className="my-4">
+          Loading <Loader2 className="animate-spin" />
+        </Button>
+      ) : (
+        <Button
+          onClick={handleRequestApprove}
+          type="button"
+          className="bg-green-700"
+          disabled={didApprove || isCompleted}
+        >
+          Approve
+        </Button>
+      )}
+    </>
+  );
 }
 
 export default RequestApproveBtn;
